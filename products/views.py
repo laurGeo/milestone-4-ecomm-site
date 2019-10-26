125ff2cf5812076e94f59b5fd82a0c34837c2f71
laurGeo <laureng5289@gmail.com>
2019-10-26 23:31:40 +0000

    Add django pagination feature

--- products/views.py
+++ products/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 3) # Show 3 contacts per page
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+ 
     return render(request, "products.html", {"products": products})

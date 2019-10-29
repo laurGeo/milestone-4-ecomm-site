54c7fa2602ba151bc4b04799605591f735a5a28e
laurGeo <laureng5289@gmail.com>
2019-10-29 23:36:19 +0000

    Migrating for editing product model

--- products/models.py
+++ products/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
-    description = models.TextField()
+    description = models.TextField(max_length=254, default='')
+    event1 = models.CharField(max_length=254, default='')
+    event2 = models.CharField(max_length=254, default='')
+    event3 = models.CharField(max_length=254, default='')
+    year = models.IntegerField(default=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 

c43125c83a4c04c2032edd0797a9f78dc433a6ab
laurGeo <laureng5289@gmail.com>
2019-10-29 22:38:20 +0000

    Adding pagination to search

--- search/views.py
+++ search/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from products.models import Product
+from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def do_search(request):
     products = Product.objects.filter(name__icontains=request.GET['q'])
+    paginator = Paginator(products, 3) # Show 3 contacts per page
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+ 
+    return render(request, "products.html", {"products": products})
     return render(request, "products.html", {"products": products})

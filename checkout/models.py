e29371f80954551d57ca7ff64ccda3b27743834b
laurGeo <laureng5289@gmail.com>
2019-10-29 13:35:49 +0000

    making changes for whitenoise static hosting

--- checkout/models.py
+++ checkout/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from products.models import Product
 
 # Create your models here.
-
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
@@ -13,15 +12,16 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
-    
+
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
-        
-        
+
+
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False)
-    product = models.ForeignKey(Product, null=False)
+    order = models.ForeignKey(Order, null=False,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
-    
+
     def __str__(self):
-        return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.product.name, self.product.price)

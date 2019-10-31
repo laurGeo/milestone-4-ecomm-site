0e6d283b33f6ff171b07d98e50b2a719a636ed2d
laurGeo <laureng5289@gmail.com>
2019-10-31 18:29:53 +0000

    Comments model

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Comment(models.Model):
+   
+    username = models.CharField(max_length=200)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+    rate = models.CharField(max_length=10)
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,14 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Comment
+from .forms import CommentForm
 
-from django.shortcuts import render
+def create_comment(request, pk=None):
 
-# Create your views here.
+    post = get_object_or_404(Comment, pk=pk) if pk else None
+    if request.method == "POST":
+        form = CommentForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            comment = form.save()
+
+    return render(request, 'product.html', {'form': comment_form})
